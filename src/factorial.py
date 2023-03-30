81f167095b24affb444650bb8694391be4ac222e
CursoJenkins0 <aprendizajeml3@gmail.com>
2023-03-30 04:19:33 -0300

    Commit con comentarios ingresados en el programa

--- src/factorial.py
+++ src/factorial.py
@@ -5,7 +5,12 @@
 #* Dr.P.E.Colla (c) 2022                                                   *
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
+
+#importamos la libreria sys
 import sys
+
+
+#Funcion factorial
 
 def factorial(num):
     if num < 0:
@@ -19,13 +24,20 @@ def factorial(num):
             num -= 1
         return fact
 
+#Definimos la condicion de que si no se paso el argumento lo pida
+
 if len(sys.argv) == 1:
     rango = input("Ingrese un rango de números separado por un guión (-) para calcular sus factoriales: ")
 else:
     rango = sys.argv[1]
 
+#Con la funcion split separamos los numeros ingresados en el argumento
+#y la funcion map los convierte en int para asigarnos a desde y hasta"
+
 desde, hasta = map(int, rango.split("-"))
 print("Calculando factoriales para el rango:", desde, "-", hasta)
+
+#El for pasa por todos los numeros ingresados y va printeando sus factoriales
 
 for num in range(desde, hasta+1):
     print("El Factorial del numero ", num, " es: ", factorial(num))
